6c6f72342d621c21517a604907e247fbadab915e
Nate Potter <ntpttr@gmail.com>
2018-10-03 17:10:07 -0700

    Update group model tests to reflect changes

--- test/server/models/test_group.js
+++ test/server/models/test_group.js
@@ -2,6 +2,7 @@ const expect = require('chai').expect;
 const mongoose = require('mongoose');
 const utils = require('../../utils');
 
+const Game = require('../../../server/models/Game');
 const Group = require('../../../server/models/Group');
 const User = require('../../../server/models/User');
 
@@ -12,39 +13,66 @@ var testUser = {
     'password': 'Password123'
 };
 
-// Group model
-var testGroup = {
-    'name': 'testgroup'
+// Game model
+var testGame = {
+    'name': 'testgame'
 };
 
 describe('group', function() {
 
     it('should be invalid if name is empty', function() {
-        var g = new Group({});
-        g.validate(function(err) {
+        var group = new Group({});
+        group.validate(function(err) {
             expect(err.errors.name).to.exist;
         });
     });
 
     it('should add users to its user list', function(done) {
-        var u = new User(testUser);
-        var g = new Group(testGroup);
-        expected = [u._id];
-
-        g.addUser(u._id);
-        expect(g.users).to.eql(expected);
+        var user = new User(testUser);
+        var group = new Group({
+            'name': 'testgroup'
+        });
+        expected = user._id;
+        group.addUser(user._id);
+        expect(group.users[0].user).to.eql(expected);
         done();
     });
 
-    it('should delete a user from group list', function(done) {
-        var u = new User(testUser);
-        var g = new Group(testGroup);
+    it('should delete a user from user list', function(done) {
+        var user = new User(testUser);
+        var group = new Group({
+            'name': 'testgroup',
+            'users': [{
+                'user': user._id,
+                'stats': []
+            }]
+        });
         expected = [];
-
-        g.deleteUser(u._id);
-
-        expect(g.users).to.eql(expected);
+        group.deleteUser(user._id);
+        expect(group.users).to.eql(expected);
         done()
     });
 
+    it('should add games to its game list', function (done) {
+        var game = new Game(testGame);
+        var group = new Group({
+            'name': 'testgroup'
+        });
+        expected = [game._id]
+        group.addGame(game._id);
+        expect(group.games).to.eql(expected);
+        done();
+    });
+
+    it('should delete games from its game list', async function (done) {
+        var game = new Game(testGame);
+        var group = new Group({
+            'name': 'testgroup',
+            'games': [game._id]
+        });
+        expected = [];
+        group.deleteGame(game._id);
+        expect(group.games).to.eql(expected);
+        done()
+    });
 });
