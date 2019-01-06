e4d6c72d45b52f6527ee89291f020be278f1652a
Nate Potter <ntpttr@gmail.com>
2019-01-06 15:07:32 -0800

    Use dependency injection for controllers and schemas

--- server/routes/gameRoutes.js
+++ server/routes/gameRoutes.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const router = new express.Router();
 
-const gameController = require('../controllers/gameController');
-
 // List all games in the DB
 router.get('/', function(req, res) {
-  gameController.getGamesDb(function(getRes) {
+  const gameSchema = req.gameSchema;
+  const gameController = req.gameController;
+
+  gameController.getGamesDb(gameSchema, function(getRes) {
     res.json(getRes);
   });
 });
 
 // Get specific game from the DB
 router.get('/:ident', function(req, res) {
+  const gameSchema = req.gameSchema;
+  const gameController = req.gameController;
   const ident = req.params.ident;
-  gameController.getGameDb(ident, function(getRes) {
+
+  gameController.getGameDb(gameSchema, ident, function(getRes) {
     res.json(getRes);
   });
 });
 
 // Get specific game from BoardGameGeek's API
 router.get('/bgg/:name', function(req, res) {
+  const gameController = req.gameController;
   const name = req.params.name;
+
   gameController.getGameBgg(name, function(getRes) {
     res.json(getRes);
   });
@@ -28,23 +34,33 @@ router.get('/bgg/:name', function(req, res) {
 
 // Save custom game details to database
 router.post('/', function(req, res) {
+  const gameSchema = req.gameSchema;
+  const gameController = req.gameController;
   const properties = req.body;
-  gameController.saveCustomGame(properties, function(saveRes) {
+
+  gameController.saveCustomGame(gameSchema, properties, function(saveRes) {
     res.json(saveRes);
   });
 });
 
 // Save bgg game details to database
 router.post('/bgg', function(req, res) {
+  const gameSchema = req.gameSchema;
+  const gameController = req.gameController;
   const name = req.body.name;
-  gameController.saveBggGame(name, function(saveRes) {
+
+  gameController.saveBggGame(gameSchema, name, function(saveRes) {
     res.json(saveRes);
   });
 });
 
 // Delete game from the DB
 router.delete('/:ident', function(req, res) {
-  gameController.deleteGame(req.params.ident, function(deleteRes) {
+  const gameSchema = req.gameSchema;
+  const gameController = req.gameController;
+  const ident = req.params.ident;
+
+  gameController.deleteGame(gameSchema, ident, function(deleteRes) {
     res.json(deleteRes);
   });
 });
