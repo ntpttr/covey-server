34048bf70a74515eb72e790a05986ad7c8768dc8
Nate Potter <ntpttr@gmail.com>
2019-01-06 15:29:55 -0800

    Fix eslint in tests

--- test/server/models/test_user.js
+++ test/server/models/test_user.js
@@ -4,55 +4,53 @@ const Group = require('../../../server/models/Group');
 const User = require('../../../server/models/User');
 
 // Group model
-var testGroup = {
-    'name': 'testgroup'
+const testGroup = {
+  'name': 'testgroup',
 };
 
 describe('user', function() {
-
-    it('should be invalid if name is empty', function() {
-        var user = new User({
-            'password': 'pass'
-        });
-        user.validate(function(err) {
-            expect(err.errors.name).to.exist;
-        });
+  it('should be invalid if name is empty', function() {
+    const user = new User({
+      'password': 'pass',
     });
-
-    it('should be invalid if password is empty', function() {
-        var user = new User({
-            'name': 'testuser'
-        });
-        user.validate(function(err) {
-            expect(err.errors.password).to.exist;
-        });
+    user.validate(function(err) {
+      expect(err.errors.name).to.exist;
     });
+  });
 
-    it('should add groups to its group list', function(done) {
-        var user = new User({
-            'name': 'testuser',
-            'password': 'pass'
-        });
-        var group = new Group(testGroup);
-        expected = [group._id];
-        user.addGroup(group._id);
-        expect(user.groups).to.eql(expected);
-        done();
+  it('should be invalid if password is empty', function() {
+    const user = new User({
+      'name': 'testuser',
     });
-
-    it('should delete a group from group list', function(done) {
-        var group = new Group(testGroup);
-        var user = new User({
-            'name': 'testuser',
-            'password': 'pass',
-            'groups': [group._id]
-        });
-        expected = [];
-
-        user.deleteGroup(group._id);
-
-        expect(user.groups).to.eql(expected);
-        done()
+    user.validate(function(err) {
+      expect(err.errors.password).to.exist;
     });
+  });
 
+  it('should add groups to its group list', function(done) {
+    const user = new User({
+      'name': 'testuser',
+      'password': 'pass',
+    });
+    const group = new Group(testGroup);
+    const expected = [group._id];
+    user.addGroup(group._id);
+    expect(user.groups).to.eql(expected);
+    done();
+  });
+
+  it('should delete a group from group list', function(done) {
+    const group = new Group(testGroup);
+    const user = new User({
+      'name': 'testuser',
+      'password': 'pass',
+      'groups': [group._id],
+    });
+    expected = [];
+
+    user.deleteGroup(group._id);
+
+    expect(user.groups).to.eql(expected);
+    done();
+  });
 });
