a7dd2b551ad4154897e2ee3cd83f632d0ac801b3
Nate Potter <ntpttr@outlook.com>
2019-11-22 18:25:35 -0800

    Handle unauthorized JWT errors

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ const router = express.Router();
 
 router.use('/', require('./api'));
 
+router.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({
+      'message': 'Invalid token.',
+    });
+  }
+});
+
 module.exports = router;
